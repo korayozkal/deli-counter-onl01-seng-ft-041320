37c50ee16ca099e3bf8db7fc61677d18c9b2ccf3
Koray Ozkal <koryozkal@gmail.com>
2020-04-19 02:53:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,3 +4,13 @@ def line(katz_deli)
    position = 1 
    queue = []
 
+if katz_deli.length == 0     
+      puts "The line is currently empty."   
+   else 
+      katz_deli.each do |customer|     
+      queue.push("#. #")     
+      position +=1   
+   end   
+   puts "The line is currently: #{queue.join(" ")}"   
+   end 
+end
