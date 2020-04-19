2146d3df2a57c2c169a577dd13a34af9c9ae8e73
Koray Ozkal <koryozkal@gmail.com>
2020-04-19 02:13:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+if katz_deli.length == 0
+puts "The line is currently empty."
+
+elsif katz_deli.length  > 0 
+counter = 1
+
+
+
+brothers = ["Tim", "Tom", "Jim"]
+counter = 1
+brothers.each do |brother|
+  puts "This is loop number #{counter}"
+  puts "Stop hitting yourself #{brother}!"
+  counter += 1
+end
+
+
+
+
+
+end
+end
+
+
