c1aa4db45574706210c5c5201f54201339cf47ca
Koray Ozkal <koryozkal@gmail.com>
2020-04-19 02:44:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,116 +1,27 @@
-
 katz_deli = []
 
-def line (katz_deli)
-  if katz_deli.size == 0
+def line(katz_deli)  
+  if katz_deli.empty? 
     puts "The line is currently empty."
   else
-    current =  "The line is currently:"
-    katz_deli.each_with_index do |name, index|
-      current.concat " #{index+1}. #{name}"
-    end
-    puts current
+    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}" 
   end
 end
-def get_customer
-  @customer = @customer || 0
-end
 
-def get_a_number(queue)
-  get_customer
-  #@customer+=1
-  queue.push(@customer+=1)
-
-end
-
-def take_a_number (katz_deli, name)
-  katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+def take_a_number(katz_deli,person)
+  if katz_deli.empty? 
+    katz_deli.push (person)
+    puts "Welcome, #{person}. You are number 1 in line."
+  else 
+    katz_deli.push (person)
+    puts "Welcome, #{person}. You are number 4 in line."
+  end
 end
 
 def now_serving(katz_deli)
-  if katz_deli.size == 0
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    next_person = katz_deli.shift
-    puts "Currently serving #{next_person}."
-  end
-end 
- 76  test.rb 
-Viewed
-@@ -0,0 +1,76 @@
-describe "Additional Tests" do
-  context "#get_a_number" do
-     it "adds newest customer to the end of the queue" do
-      queue = []
-      get_a_number(queue)
-      expect(queue).to eq([1])
-
-      get_a_number(queue)
-      expect(queue).to eq([1,2])
-    end
-
-    it "returns the customer's ticket number" do
-      queue = []
-      bob = get_a_number(queue)
-      expect(bob).to eq(1)
-
-      alice = get_a_number(queue)
-      expect(alice).to eq(2)
-    end
-  end
-
-  context "#serve_customer" do
-    it "knows how to take people off of the queue" do
-      queue = []
-      get_a_number(queue)
-      get_a_number(queue)
-
-      expect(serve_customer(queue)).to eq(1)
-
-      expect(queue).to eq([2])
-    end
-  end
-
-  context "Busy Store" do
-    it "can deal with a busy store" do
-      queue = []
-      get_a_number(queue)
-      get_a_number(queue)
-
-      serve_customer(queue)
-
-      get_a_number(queue)
-      expect(queue).to eq([2,3])
-    end
-
-    it "can deal with a store's customer waves" do
-      queue = []
-      get_a_number(queue)
-      get_a_number(queue)
-      serve_customer(queue)
-      serve_customer(queue)
-      expect(queue).to eq([])
-      # nothing for 3 hours
-
-      get_a_number(queue)
-      get_a_number(queue)
-      expect(queue).to eq([3,4])
-    end
-
-    it "can deal with 2 stores" do
-      deli1 = []
-      get_a_number(deli1)
-      get_a_number(deli1)
-      serve_customer(deli1)
-      expect(deli1).to eq([2])
-
-      deli2 = []
-      get_a_number(deli2)
-      get_a_number(deli2)
-      get_a_number(deli2)
-      get_a_number(deli2)
-      serve_customer(deli2)
-      expect(deli2).to eq([2,3,4])
-    end
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
   end
