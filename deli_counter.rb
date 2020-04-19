322ed19137d4e447f3c3ba7ab1d5ebff050be2eb
Koray Ozkal <koryozkal@gmail.com>
2020-04-19 02:53:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,17 @@ if katz_deli.length == 0
    puts "The line is currently: #{queue.join(" ")}"   
    end 
 end
+
+def take_a_number(katz_deli, customer)   
+   katz_deli.push(customer)   
+   puts "Welcome, #. You are number # in line." 
+end  
+
+def now_serving(katz_deli)   
+   if katz_deli.empty?     
+      puts "There is nobody waiting to be served!"   
+   else     
+      puts "Currently serving #."   
+      katz_deli.shift 
+   end 
+end
