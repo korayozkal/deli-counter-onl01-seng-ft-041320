7aa791c3a092d0cfeb0db4d5dbba79318e3fd528
Koray Ozkal <koryozkal@gmail.com>
2020-04-19 02:55:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,30 +1,25 @@
+katz_deli = []
 
-katz_deli = []  
-def line(katz_deli) 
-   position = 1 
-   queue = []
-
-if katz_deli.length == 0     
-      puts "The line is currently empty."   
-   else 
-      katz_deli.each do |customer|     
-      queue.push("#. #")     
-      position +=1   
-   end   
-   puts "The line is currently: #{queue.join(" ")}"   
-   end 
+def line(x)
+  line_array = []
+  if x.length == 0
+    puts "The line is currently empty."
+  else
+    x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
+  end
 end
-
-def take_a_number(katz_deli, customer)   
-   katz_deli.push(customer)   
-   puts "Welcome, #. You are number # in line." 
-end  
-
-def now_serving(katz_deli)   
-   if katz_deli.empty?     
-      puts "There is nobody waiting to be served!"   
-   else     
-      puts "Currently serving #."   
-      katz_deli.shift 
-   end 
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
 end
