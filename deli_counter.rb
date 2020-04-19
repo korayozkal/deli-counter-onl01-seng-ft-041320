edf7288343d00dcdc981be9405c605875b70d653
Koray Ozkal <koryozkal@gmail.com>
2020-04-19 02:44:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli = []
 
 def line(katz_deli)  
-  if katz_deli.empty? 
+  if katz_deli == 0 
     puts "The line is currently empty."
   else
     puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}" 
