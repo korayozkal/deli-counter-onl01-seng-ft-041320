d1222c1366065792d9d2f738131aaaa0e90f81f3
Koray Ozkal <koryozkal@gmail.com>
2020-04-19 02:29:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,6 @@ def line(katz_deli)
       current.concat " #{index+1}. #{name}"
 		puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
 	end
-	
 end
 
 def take_a_number(katz_deli, name)
